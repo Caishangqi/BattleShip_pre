ebdb68b1925e3bd76eef27cea5bd8791015cac6f
Jinguang Chai <cjgclzmr@gmail.com>
2020-02-03 23:22:15 -0800

    Change List to tuple

--- main.py
+++ main.py
@@ -8,11 +8,12 @@ if __name__ == '__main__':
     for line in file.readlines():
         line = line.strip('\n')
         configuration_file.append(line)
-    dimension = configuration_file[0].split(" ")
-    for element in dimension:
-        element = int(element)
+    dimension = configuration_file[0].split()
+    for i in range(len(dimension)):
+        dimension[i] = int(dimension[i])
     for ships in range(1, len(configuration_file)):
-        ships_in_list.append(ships)
+        index, element = configuration_file[ships].split()
+        ships_in_list.append((index[0], int(element)))
     # Game need input dimension and list of ships
     game1 = Game(dimension, ships_in_list)
     game1.play()   #Test
