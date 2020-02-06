8094fc2caaf2080b26a79f326b335ee42e6a3fc6
caishangqi <jack2001714@126.com>
2020-02-06 15:19:52 +0800

    Add ship

--- BattleShip/scr/Ship.py
+++ BattleShip/scr/Ship.py
@@ -1,7 +1,12 @@
-from typing import List, Dict
+from typing import List, Dict, Iterable
+from .Board import Board
 
 
 class Ship(object):
-    def __init__(self):
-        pass
+    def __init__(self, orientaion_point: list[int], boat_name: str):
+        self.orientation_point = orientaion_point
+        self.boat_cover_point = []
+        self.boat_name = boat_name
 
+    def print_boat_name(self) -> str:
+        return self.boat_name
